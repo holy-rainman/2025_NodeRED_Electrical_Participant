1df473c9962ad7de0ce0afd7611b995196dfe581
Abd Rahim Kasiman <holy_rainman@yahoo.com>
2025-09-01 22:18:03 +0800

    Create PB1_RiseFallEdge.ino

---
+++ PB1_RiseFallEdge.ino
@@ -0,0 +1,36 @@
+#define LEDR  15
+#define LEDG  2
+#define RLY1  23
+#define RLY2  22
+#define PB1   18
+#define PB2   19
+
+void setup() 
+{ pinMode(LEDR,OUTPUT);
+  pinMode(LEDG,OUTPUT);
+  pinMode(RLY1,OUTPUT);
+  pinMode(RLY2,OUTPUT);
+  digitalWrite(RLY1,HIGH);
+  digitalWrite(RLY2,HIGH);
+  
+  pinMode(PB1,INPUT_PULLUP);
+  pinMode(PB2,INPUT_PULLDOWN);
+}
+void loop() 
+{ if(digitalRead(PB1)==0) 
+  { digitalWrite(RLY1,LOW);  delay(500);
+    
+    while(digitalRead(PB1)==0)
+    { digitalWrite(RLY1,HIGH);
+      digitalWrite(LEDR,HIGH);
+    }
+    
+    digitalWrite(LEDR,LOW);
+    digitalWrite(LEDG,HIGH);
+    delay(1000);
+  }
+  if(digitalRead(PB1)==1 && digitalRead(PB2)==0)
+  { digitalWrite(RLY1,HIGH);  digitalWrite(LEDR,LOW);
+    digitalWrite(RLY2,HIGH);  digitalWrite(LEDG,LOW);  
+  }
+}
