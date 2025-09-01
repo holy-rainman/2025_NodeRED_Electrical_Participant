a1cf6cc2126f6d088ee4e8beaed0853fe60e94e5
Abd Rahim Kasiman <holy_rainman@yahoo.com>
2025-09-01 21:57:01 +0800

    Create Relay_Basics.ino

---
+++ Relay_Basics.ino
@@ -0,0 +1,19 @@
+#define LEDR  15
+#define LEDG  2
+#define RLY1  23
+#define RLY2  22
+
+void setup() 
+{ pinMode(LEDR,OUTPUT);
+  pinMode(LEDG,OUTPUT);
+  pinMode(RLY1,OUTPUT);
+  pinMode(RLY2,OUTPUT);
+  digitalWrite(RLY1,HIGH);
+  digitalWrite(RLY2,HIGH);
+}
+void loop() 
+{ digitalWrite(RLY1,LOW);   delay(500);
+  digitalWrite(RLY1,HIGH);  delay(500);
+  digitalWrite(RLY2,LOW);   delay(500);
+  digitalWrite(RLY2,HIGH);  delay(500);
+}
