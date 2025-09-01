2055d83bcf29bb62a102a44184eb50a2e8b93143
Abd Rahim Kasiman <holy_rainman@yahoo.com>
2025-09-01 21:55:04 +0800

    Create LEDRG.ino

---
+++ LEDRG.ino
@@ -0,0 +1,15 @@
+#define LEDR  15
+#define LEDG  2
+
+void setup() 
+{ pinMode(LEDR,OUTPUT);
+  pinMode(LEDG,OUTPUT);
+}
+void loop() 
+{ digitalWrite(LEDR,HIGH); 
+  digitalWrite(LEDG,LOW); 
+  delay(500);
+  digitalWrite(LEDR,LOW); 
+  digitalWrite(LEDG,HIGH); 
+  delay(500);
+}
