2aa6ca0d1565bb1afe051621e9afd1c1c057fe4d
Abd Rahim Kasiman <holy_rainman@yahoo.com>
2025-09-03 14:48:42 +0800

    Create NodeRED_AllOutput.ino

---
+++ NodeRED_AllOutput.ino
@@ -0,0 +1,92 @@
+#include <WiFi.h>
+#include <WiFiClientSecure.h>
+#include <PubSubClient.h>
+
+#define LEDR 2
+#define LEDG 15
+#define RLY1 23
+#define RLY2 22
+
+// ==== WiFi credentials ====
+const char* ssid        = "YOUR_SSID";
+const char* password    = "YOUR_PW";
+
+// ==== HiveMQ Cloud credentials ====
+const char* mqtt_server = "YOUR_URL_SERVER";
+const int   mqtt_port   = 8883;
+const char* mqtt_user   = "YOUR SERVER_USERNAME";
+const char* mqtt_pass   = "YOUR_SERVER_PW";
+WiFiClientSecure espClient;
+PubSubClient client(espClient);
+
+void setup_wifi() 
+{ Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) 
+  { delay(500);
+    Serial.print(".");
+  }
+  Serial.println("\nWiFi connected");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void callback(char* topic, byte* payload, unsigned int length) 
+{ String msg = "";
+  for (int i = 0; i < length; i++) 
+    msg += (char)payload[i];
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("]: ");
+  Serial.println(msg);
+
+  if (msg == "ledr_1") digitalWrite(LEDR, HIGH);
+  if (msg == "ledr_0") digitalWrite(LEDR, LOW);
+  if (msg == "ledg_1") digitalWrite(LEDG, HIGH);
+  if (msg == "ledg_0") digitalWrite(LEDG, LOW);
+
+  if (msg == "rly1_1") digitalWrite(RLY1, LOW);
+  if (msg == "rly1_0") digitalWrite(RLY1, HIGH);
+  if (msg == "rly2_1") digitalWrite(RLY2, LOW);
+  if (msg == "rly2_0") digitalWrite(RLY2, HIGH);
+}
+
+void reconnect() 
+{ while (!client.connected()) 
+  { Serial.print("Attempting MQTT connection...");
+    if (client.connect("ESPClient", mqtt_user, mqtt_pass)) 
+    { Serial.println("connected");
+      client.subscribe("aim/LedControl");
+      client.subscribe("aim/RlyControl");
+    } 
+    else 
+    { Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      delay(5000);
+    }
+  }
+}
+
+void setup() 
+{ Serial.begin(115200);
+  setup_wifi();
+
+  espClient.setInsecure();
+  client.setServer(mqtt_server, mqtt_port);
+  client.setCallback(callback);
+
+  pinMode(LEDR,OUTPUT);  digitalWrite(LEDR,LOW);
+  pinMode(LEDG,OUTPUT);  digitalWrite(LEDG,LOW);
+
+  pinMode(RLY1,OUTPUT);  digitalWrite(RLY1,HIGH);
+  pinMode(RLY2,OUTPUT);  digitalWrite(RLY2,HIGH);
+}
+
+void loop() 
+{ if (!client.connected()) 
+    reconnect();
+  client.loop();
+}
